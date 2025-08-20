b68856df65002a552c8426aa92849b3afdea6378
rain <154728367+rain-cassiopeia@users.noreply.github.com>
2025-08-20 14:38:43 -0400

    goto fonemode

--- script.js
+++ script.js
@@ -53,3 +53,13 @@ function getOffset() {
 
 window.addEventListener('load', rigorousWindowHeight);
 window.addEventListener('resize', rigorousWindowHeight);
+
+function checkWindowSize() {
+  if (window.innerWidth <= 600) {
+    window.location.href = "/fone/mode";
+  }
+}
+
+checkWindowSize();
+
+window.addEventListener('resize', checkWindowSize);
